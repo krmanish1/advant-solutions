9da0c6ea8976aff6afb6d9c75891e20e155f0ecd
Manish Kumar <138682286+krmanish1@users.noreply.github.com>
2024-11-15 12:43:36 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Banner1 from "@/Components/Banner1";
 import CaseStudy from "@/Components/caseStudy/CaseStudy";
+import Features from "@/Components/Features";
 import Footer from "@/Components/Footer";
 import HeroSection from "@/Components/herosection/HeroSection";
 import Newsletter from "@/Components/Newsletter";
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex flex-col w-full">
       <HeroSection />
-
+      <Features />
       {/* <Services /> */}
       <ServicesStep />
       {/* <OurTeam /> */}
